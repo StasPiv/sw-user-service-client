377dac9944c38b470b57469d1a554e1c3966d474
Stanislav Pivovartsev <stanislav.pivovartsev@prym.com>
2022-07-26 15:39:25 +0300

    remove debug

--- src/UserServiceClient.php
+++ src/UserServiceClient.php
@@ -16,8 +16,6 @@ class UserServiceClient implements UserServiceClientInterface
         $ch = curl_init();
         $url = $this->userServiceUrl . 'create.php';
         $dataForPost = array_merge(['owner_id' => $ownerId], $data);
-        var_dump($dataForPost);
-        var_dump($url);
 
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, 1);
